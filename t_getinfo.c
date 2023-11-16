6b51ecb4edf59c41c74d01b2d6f4bd3c60b5f150
Mafuyeka <tshepisoroshina@gmail.com>
2023-11-16 02:58:00 -0800

    t_getinfo.c

--- t_getinfo.c
+++ t_getinfo.c
@@ -21,21 +21,21 @@ int i = 0;
 info->fname = av[0];
 if (info->arg)
 {
-info->argv = strtow(info->arg, " \t");
+info->argv = strtok(info->arg, " \t");
 if (!info->argv)
 {
 info->argv = malloc(sizeof(char *) * 2);
 if (info->argv)
 {
-info->argv[0] = _strdup(info->arg);
+info->argv[0] = t_strdup(info->arg);
 info->argv[1] = NULL;
 }
 }
 for (i = 0; info->argv && info->argv[i]; i++)
 ;
 info->argc = i;
-replace_alias(info);
-replace_vars(info);
+t_replace_alias(info);
+t_replace_vars(info);
 }
 }
 /**
@@ -45,7 +45,7 @@ replace_vars(info);
  */
 void t_free_info(info_t *info, int all)
 {
-ffree(info->argv);
+free(info->argv);
 info->argv = NULL;
 info->path = NULL;
 if (all)
@@ -53,16 +53,16 @@ if (all)
 if (!info->cmd_buf)
 free(info->arg);
 if (info->env)
-free_list(&(info->env));
+t_free_list(&(info->env));
 if (info->history)
 free_list(&(info->history));
 if (info->alias)
 free_list(&(info->alias));
 ffree(info->environ);
 info->environ = NULL;
-bfree((void **)info->cmd_buf);
+free((void **)info->cmd_buf);
 if (info->readfd > 2)
 close(info->readfd);
-_putchar(BUF_FLUSH);
+t_putchar(BUF_FLUSH);
 }
 }
